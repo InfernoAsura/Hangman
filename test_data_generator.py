9818f86ccaadc26abd2349050dfd81f1adbdb23d
InfernoAsura <126259131+InfernoAsura@users.noreply.github.com>
2024-10-09 23:54:06 +0530

    Add files via upload

---
+++ test_data_generator.py
@@ -0,0 +1,16 @@
+import random
+import nltk 
+from nltk.corpus import words
+
+nltk.download('words')
+
+word_list = words.words()
+
+num_words = 100
+
+random_words = random.choices(word_list, k=num_words)
+
+with open('random_words.txt', 'w') as f:
+    for word in random_words:
+        f.write(word + '\n')
+
